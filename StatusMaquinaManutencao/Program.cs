bee5bf369204b72143a6a70bed775e22c271ab4a
Cristian <cris.ferreirak10@gmail.com>
2023-08-08 13:54:50 -0300

    testando mandar os relatorios por emails no thunderbird

--- StatusMaquinaManutencao/Program.cs
+++ StatusMaquinaManutencao/Program.cs
@@ -1,37 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Net;
+using System.Net.Mail;
 using Telegram.Bot;
 using Telegram.Bot.Args;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
-namespace TelegramBotExample
-{
-    class Program
-    {
+namespace TelegramBotExample {
+    class Program {
         private static TelegramBotClient botClient;
         private static Dictionary<long, string> machineStatus = new Dictionary<long, string>();
 
-        static void Main()
-        {
-            botClient = new TelegramBotClient("6349800967:AAGCqfz7BVIAI-nTtg38K06h6k0EPzNE9GI");
+        static void Main() {
+            botClient = new TelegramBotClient("6443939914:AAG0BMQ0t_svMoqKqeAALnYLWD0b1OrvnxQ");
             botClient.OnMessage += Bot_OnMessage;
             botClient.StartReceiving();
+            ScheduleDailyTask();
             Console.ReadLine();
             botClient.StopReceiving();
         }
 
-        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Type == MessageType.Text)
-            {
+        private static void SendBackupByEmail(string filePath, List<string> recipientAddresses) {
+            try {
+                var fromAddress = new MailAddress("matheus.adryan@mirvibrasil.com", "matheus.adryan@mirvibrasil.com");
+
+                string subject = "backup do dia " + DateTime.Now.ToString("dd-MM-yyyy");
+                string body = "Este é o backup diário dos dados.\nPor favor, verifique o anexo para mais detalhes.";
+
+                var smtpClient = new SmtpClient {
+                    Host = "smtp1.mirvibrasil.com",
+                    Port = 25,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, "")
+                };
+
+                using (var message = new MailMessage { From = fromAddress, Subject = subject, Body = body }) {
+                    foreach (var recipientAddress in recipientAddresses) {
+                        message.To.Add(new MailAddress(recipientAddress));
+                    }
+
+                    message.Attachments.Add(new Attachment(filePath));
+                    smtpClient.Send(message);
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Error sending email: {ex.Message}");
+            }
+        }
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e) {
+            if (e.Message.Type == MessageType.Text) {
                 var chatId = e.Message.Chat.Id;
                 var messageText = e.Message.Text;
 
-                if (messageText == "/Comecar" || messageText == "/c")
-                {
-                    // Criação dos grupos de nomes de máquinas
+                if (messageText == "/Comecar") {
                     var machineGroups = new List<List<string>>
                     {
                         new List<string> {"HC 073", "HC 077", "HC 070", "HC 065"},
@@ -40,33 +62,25 @@ namespace TelegramBotExample
                         new List<string> {"LC 579", "LC 577", "LC 574", "LC 564"}
                     };
 
-                    // Criação dos botões para cada grupo
                     var keyboardRows = new List<List<KeyboardButton>>();
-                    foreach (var group in machineGroups)
-                    {
+                    foreach (var group in machineGroups) {
                         var row = new List<KeyboardButton>();
-                        foreach (var machineName in group)
-                        {
+                        foreach (var machineName in group) {
                             row.Add(new KeyboardButton(machineName));
                         }
                         keyboardRows.Add(row);
                     }
 
-                    // Criação do teclado personalizado com os botões
                     var keyboard = new ReplyKeyboardMarkup(keyboardRows, resizeKeyboard: true, oneTimeKeyboard: true);
 
-                    // Envia a mensagem com o teclado personalizado
                     await botClient.SendTextMessageAsync(chatId, "Selecione uma máquina:", replyMarkup: keyboard);
                 }
-                else if (machineStatus.ContainsKey(chatId))
-                {
+                else if (machineStatus.ContainsKey(chatId)) {
                     string machineName = machineStatus[chatId];
 
-                    // Separar as informações usando o separador "*"
                     string[] parts = messageText.Split(new string[] { " * " }, StringSplitOptions.None);
 
-                    if (parts.Length >= 2)
-                    {
+                    if (parts.Length >= 2) {
                         string status = parts[0];
                         string additionalInfo = parts[1];
 
@@ -77,47 +91,22 @@ namespace TelegramBotExample
 
                         await botClient.SendTextMessageAsync(chatId, message, replyToMessageId: e.Message.MessageId);
                     }
-                    else
-                    {
+                    else {
                         await botClient.SendTextMessageAsync(chatId, "Status falha: ");
                     }
                 }
-                else if (IsValidMachineName(messageText))
-                {
+                else if (IsValidMachineName(messageText)) {
                     machineStatus[chatId] = messageText;
 
                     await botClient.SendTextMessageAsync(chatId, "Status falha: ");
                 }
-                else if (messageText.Contains(" * "))
-                {
-                    // Separar as informações usando o separador "*"
-                    string[] parts = messageText.Split(new string[] { " * " }, StringSplitOptions.None);
-
-                    if (parts.Length >= 3)
-                    {
-                        string machineName = parts[0];
-                        string status = parts[1];
-                        string additionalInfo = parts[2];
-
-                        string currentTime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
-                        string message = $"Maq.: {machineName}\nStatus: {status}\nDescricao: {additionalInfo}\nTime: {currentTime}";
-
-                        await botClient.SendTextMessageAsync(chatId, message, replyToMessageId: e.Message.MessageId);
-                    }
-                    else
-                    {
-                        await botClient.SendTextMessageAsync(chatId, "Formato inválido. Por favor, informe os parâmetros no formato 'Nome da Máquina * Status * Descrição'.");
-                    }
-                }
-                else
-                {
-                    await botClient.SendTextMessageAsync(chatId, "Por favor, selecione uma máquina válida ou informe os parâmetros no formato 'Nome da Máquina * Status * Descrição'.");
+                else {
+                    await botClient.SendTextMessageAsync(chatId, "Por favor, selecione uma máquina válida.");
                 }
             }
         }
 
-        private static bool IsValidMachineName(string name)
-        {
+        private static bool IsValidMachineName(string name) {
             var validMachineNames = new List<string>
             {
                 "HC 073", "HC 077", "HC 070", "HC 065",
@@ -127,6 +116,57 @@ namespace TelegramBotExample
             };
 
             return validMachineNames.Contains(name);
+        }
+
+        private static void PerformDailyTask(object state) {
+            var backupFolder = "C:\\Users\\Cristian\\PastaBackup";
+            var backupFilename = $"Backup_{DateTime.Now:yyyyMMdd}.txt";
+
+            var backupData = new List<string>();
+
+            foreach (var entry in machineStatus) {
+                backupData.Add(entry.Value);
+            }
+
+            File.WriteAllLines(Path.Combine(backupFolder, backupFilename), backupData);
+
+            List<string> recipients = new List<string>
+                {
+                    "lucas.william@mirvibrasil.com"
+                };
+
+            SendBackupByEmail(Path.Combine(backupFolder, backupFilename), recipients);
+            try {
+                var smtpClient = new SmtpClient("smtp1.mirvibrasil.com") {
+                    Port = 25,
+                    Credentials = new NetworkCredential("matheus.adryan@mirvibrasil.com", ""),
+                    EnableSsl = true,
+                };
+
+                var mail = new MailMessage {
+                    From = new MailAddress("matheus.adryan@mirvibrasil.com"),
+                    Subject = "Backup de Dados Diário",
+                    Body = "Anexos: Dados do Backup",
+                };
+
+                foreach (var recipientAddress in recipients) {
+                    mail.To.Add(recipientAddress);
+                }
+
+                mail.Attachments.Add(new Attachment(Path.Combine(backupFolder, backupFilename)));
+
+                smtpClient.Send(mail);
+            }
+            catch (Exception ex) {
+                Console.WriteLine("Erro ao enviar e-mail: " + ex.Message);
+            }
+        }
+
+        private static void ScheduleDailyTask() {
+            var now = DateTime.Now;
+            var timeUntilMidnight = TimeSpan.FromMinutes(1);
+
+            var timer = new Timer(PerformDailyTask, null, timeUntilMidnight, TimeSpan.FromMinutes(1));
         }
     }
 }
